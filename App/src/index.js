a3e31ef641baebe51005fba4feff84fc557af21d
Aldo Junior <aldojunior@Aldos-MacBook-Pro.local>
2018-06-02 14:33:50 -0300

    Implementação do redux

--- App/src/index.js
+++ App/src/index.js
@@ -1,7 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import {Provider} from 'react-redux'
+import {createStore, combineReducers, applyMiddleware} from 'redux'
+import thunkMiddleweare from 'redux-thunk';
+import {reducer as reduxFormReducer} from 'redux-form'
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+try {
+  localStorage.setItem('test', 'test');
+  localStorage.removeItem('test');
+} catch (e) {
+  alert("Por favor, ative os coockies de seu navegador, para continuar a navegação.");
+}
+
+const reducer = combineReducers({
+  form: reduxFormReducer,
+});
+
+let store;
+store = createStore(reducer, applyMiddleware(thunkMiddleweare));
+
+const dest = document.getElementById('root');
+ReactDOM.render(
+  (
+    <Provider store={store}>
+      <App/>
+    </Provider>
+  ), dest);
+
 registerServiceWorker();
