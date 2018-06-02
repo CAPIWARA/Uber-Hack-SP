219f84515a193632ac652d95fda167f1aad35b6d
Vitor Luiz Cavalcanti <vitorluizc@outlook.com>
2018-06-02 20:59:45 -0300

    Add SlippySpinner

---
+++ App/src/components/Slippy/SlippySpinner.js
@@ -0,0 +1,10 @@
+import './SlippySpinner.css';
+import React from 'react';
+
+function SlippySpinner (props) {
+  return (
+    <figure className="SlippySpinner" { ...props }></figure>
+  );
+}
+
+export default SlippySpinner;
