671f420d4048f9e7fdca43d461ff2c20d851553a
Aldo Junior <aldojunior@Aldos-MacBook-Pro.local>
2018-06-03 12:40:06 -0300

    area do estabelecimenti

--- estabelecimento-app/src/App.js
+++ estabelecimento-app/src/App.js
@@ -5,8 +5,48 @@ import avatar from './assets/images/profile-image.jpeg';
 import NotFound from "./views/NotFound";
 import Aside from "./components/Aside";
 import EfetuarCheckinView from "./views/EfetuarCheckinView";
+import {initializeApp, database} from 'firebase'
+
+initializeApp({
+  apiKey: "AIzaSyD-7x27r2UkmoXHX_UKkZKHbBpQSp3eGd4",
+  authDomain: "uber-hack-sp.firebaseapp.com",
+  databaseURL: "https://uber-hack-sp.firebaseio.com",
+  projectId: "uber-hack-sp",
+  storageBucket: "uber-hack-sp.appspot.com",
+  messagingSenderId: "264181005451"
+});
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      reservas: [],
+      users: {}
+    }
+  }
+
+  componentDidMount() {
+    database().goOnline();
+    database().ref('/reservations').on('value', (snapshot) => {
+      this.setState({reservas: Object.values(snapshot.val())});
+      this.fetchUsers();
+      console.log(this.state.reservas)
+    });
+  }
+
+  async fetchUsers () {
+    const ids = new Set(this.state.reservas.map((reservation) => reservation.user));
+    const users = await Promise.all([ ...ids ].map((id) => {
+      return fetch(`https://uber-hack-sp.firebaseio.com/users/${id}.json`)
+        .then((response) => response.json());
+    }));
+    this.setState({ users });
+  }
+
+  getName (id) {
+    return this.state.users && this.state.users[id] && this.state.users[id].name;
+  }
+
   render() {
     return (
       <Router>
@@ -20,72 +60,41 @@ class App extends Component {
 
             <section className="dashCenterContainer">
               <Switch>
-                <Route exact path='/efetuar-checkin' component={EfetuarCheckinView}/>
+                <Route exact path='/' component={EfetuarCheckinView}/>
 
                 <Route component={NotFound}/>
               </Switch>
             </section>
 
             <section className="dashSolicitacoesPendentes">
-              <article className="solicitacaoCard">
-                <div className="solicitacaoCardContainer">
-                  <img src={avatar} alt=""/>
 
-                  <div>
-                    <h3>Fulano</h3>
-                    <p>05/06/2018 às 09:45</p>
-                  </div>
-                </div>
-                <div className="solicitacaoCardButtons">
-                  <button className='cancelButton'>
-                    Recusar
-                  </button>
 
-                  <button className='acceptButton'>
-                    Aceitar
-                  </button>
-                </div>
-              </article>
+              {this.state.reservas.map((reserva, index) => {
+                return reserva.status === 'pending' && (
 
-              <article className="solicitacaoCard">
-                <div className="solicitacaoCardContainer">
-                  <img src={avatar} alt=""/>
+                  <article className="solicitacaoCard" key={index}>
+                    <div className="solicitacaoCardContainer">
+                      <img src={avatar} alt=""/>
 
-                  <div>
-                    <h3>Fulano</h3>
-                    <p>05/06/2018 às 09:45</p>
-                  </div>
-                </div>
-                <div className="solicitacaoCardButtons">
-                  <button className='cancelButton'>
-                    Recusar
-                  </button>
+                      <div>
+                        <h3>{this.getName(reserva.user)}</h3>
+                        <p>{reserva.start}</p>
+                      </div>
+                    </div>
+                    <div className="solicitacaoCardButtons">
+                      <button className='cancelButton'>
+                        Recusar
+                      </button>
 
-                  <button className='acceptButton'>
-                    Aceitar
-                  </button>
-                </div>
-              </article>
+                      <button className='acceptButton'>
+                        Aceitar
+                      </button>
+                    </div>
+                  </article>
+                )
+              })}
 
-              <article className="solicitacaoCard">
-                <div className="solicitacaoCardContainer">
-                  <img src={avatar} alt=""/>
 
-                  <div>
-                    <h3>Fulano</h3>
-                    <p>05/06/2018 às 09:45</p>
-                  </div>
-                </div>
-                <div className="solicitacaoCardButtons">
-                  <button className='cancelButton'>
-                    Recusar
-                  </button>
-
-                  <button className='acceptButton'>
-                    Aceitar
-                  </button>
-                </div>
-              </article>
             </section>
 
           </div>
--- estabelecimento-app/src/views/EfetuarCheckinView.js
+++ estabelecimento-app/src/views/EfetuarCheckinView.js
@@ -1,24 +1,51 @@
 import React, {Component} from 'react';
 import './EfetuarCheckinView.css';
 
-import SlippyModal from '../components/Slippy/SlippyModal'
-import SlippyButton from '../components/Slippy/SlippyButton'
+import SlippyModal from '../components/Slippy/SlippyModal';
+import SlippyButton from '../components/Slippy/SlippyButton';
+import {database} from 'firebase';
 
 export default class EfetuarCheckinView extends Component {
-  constructor(){
+  constructor() {
     super();
-    this.state = {isModalCheckInOpen: false}
+    this.state = {
+      isModalCheckInOpen: false,
+      reservas: [],
+      users: {}
+    }
   }
+
+  componentDidMount() {
+    database().goOnline();
+    database().ref('/reservations').on('value', (snapshot) => {
+      this.setState({reservas: Object.values(snapshot.val())});
+      this.fetchUsers();
+      console.log(this.state.reservas)
+    });
+  }
+
+  async fetchUsers () {
+    const ids = new Set(this.state.reservas.map((reservation) => reservation.user));
+    const users = await Promise.all([ ...ids ].map((id) => {
+      return fetch(`https://uber-hack-sp.firebaseio.com/users/${id}.json`)
+        .then((response) => response.json());
+    }));
+    this.setState({ users });
+  }
+
+  getName (id) {
+    return this.state.users && this.state.users[id] && this.state.users[id].name;
+  }
+
   render() {
     return (
       <div>
-        <h1>Efetuar Check-in</h1>
+        <h1>Reservas</h1>
 
         <table className='EfetuarCheckin__table'>
           <thead>
           <tr>
-            <th>Data</th>
-            <th>Hora</th>
+            <th>Data e Hora</th>
             <th>Duração</th>
             <th>Nome</th>
             <th>Serviços</th>
@@ -26,50 +53,28 @@ export default class EfetuarCheckinView extends Component {
           </tr>
           </thead>
           <tbody>
-          <tr>
-            <td>03/06/2018</td>
-            <td>07:30</td>
-            <td>00:30</td>
-            <td>Fulano</td>
-            <td>Bike, Ducha</td>
-            <td><button onClick={()=>this.setState({isModalCheckInOpen:true})}>Check-in</button></td>
-          </tr>
 
-          <tr>
-            <td>03/06/2018</td>
-            <td>07:30</td>
-            <td>00:30</td>
-            <td>Fulano</td>
-            <td>Bike, Ducha</td>
-            <td><button onClick={()=>this.setState({isModalCheckInOpen:true})}>Check-in</button></td>
-          </tr>
+          {this.state.reservas.map((reserva, index) => {
+            return reserva.status !== 'pending' && (
+              <tr key={index}>
+                <td>{reserva.start}</td>
+                <td>{reserva.permanence}</td>
+                <td>
+                  {this.getName(reserva.user)}
+                </td>
+                <td>{reserva.type === 'shower' ? 'Ducha' : reserva.type === 'rack' ? 'Bike' : 'Ducha, Bike'}</td>
+                <td>
+                  {!reserva.check_in ? (
+                      <button onClick={() => this.setState({isModalCheckInOpen: true})}>Check-in</button>) :
+                    !reserva.check_out ? (
+                      <button onClick={() => this.setState({isModalCheckInOpen: true})}>Check-out</button>) : ''
+                  }
 
-          <tr>
-            <td>03/06/2018</td>
-            <td>07:30</td>
-            <td>00:30</td>
-            <td>Fulano</td>
-            <td>Bike, Ducha</td>
-            <td><button onClick={()=>this.setState({isModalCheckInOpen:true})}>Check-in</button></td>
-          </tr>
+                </td>
+              </tr>
+            )
+          })}
 
-          <tr>
-            <td>03/06/2018</td>
-            <td>07:30</td>
-            <td>00:30</td>
-            <td>Fulano</td>
-            <td>Bike, Ducha</td>
-            <td><button onClick={()=>this.setState({isModalCheckInOpen:true})}>Check-in</button></td>
-          </tr>
-
-          <tr>
-            <td>03/06/2018</td>
-            <td>07:30</td>
-            <td>00:30</td>
-            <td>Fulano</td>
-            <td>Bike, Ducha</td>
-            <td><button onClick={()=>this.setState({isModalCheckInOpen:true})}>Check-in</button></td>
-          </tr>
           </tbody>
         </table>
 
