3eb202e56fe0f854b52db067ff3cc57045f5e58f
Aldo Junior <aldojunior@Aldos-MacBook-Pro.local>
2018-06-03 12:45:31 -0300

    logo

--- estabelecimento-app/src/components/Aside.js
+++ estabelecimento-app/src/components/Aside.js
@@ -1,11 +1,13 @@
 import React, {Component} from 'react';
 import {NavLink} from 'react-router-dom';
+import logo from '../assets/images/logo.svg'
 
 export default class Aside extends Component {
   render() {
     return (
       <aside className="dashAside">
         <nav>
+          <img src={logo} alt="logo" style={{width:'50px', display:'block', margin:'0 auto 20px'}}/>
           <ul>
             <li>
               <NavLink to='/' exact>Reservas</NavLink>
