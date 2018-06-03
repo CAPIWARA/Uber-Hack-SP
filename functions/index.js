05addd88de78c0ffc53454624eb8942c6321f1f8
Vitor Luiz Cavalcanti <vitorluizc@outlook.com>
2018-06-03 09:15:40 -0300

    Add cors to firebase functions

--- functions/index.js
+++ functions/index.js
@@ -2,30 +2,33 @@ const admin = require('firebase-admin');
 const functions = require('firebase-functions');
 const { discal } = require('geo-dist-calc');
 const account = require('./credentials');
+const cors = require('cors')({ origin: true });
 
 admin.initializeApp({
   credential: admin.credential.cert(account),
   databaseURL: "https://uber-hack-sp.firebaseio.com"
 });
 
-exports.search = functions.https.onRequest((request, response) => {
-  const table = admin.database().ref('/establishments');
+exports.search = functions.https.onRequest((req, res) => {
+  cors(req, res, () => {
+    const table = admin.database().ref('/establishments');
 
-  table.once('value').then((establishments) => {
-    const nearby = [];
+    table.once('value').then((establishments) => {
+      const nearby = [];
 
-    establishments.forEach((item) => {
-      const establishment = item.val();
+      establishments.forEach((item) => {
+        const establishment = item.val();
 
-      const { kilometers } = discal(establishment, {
-        latitude: +request.query.latitude,
-        longitude: +request.query.longitude
+        const { kilometers } = discal(establishment, {
+          latitude: +req.query.latitude,
+          longitude: +req.query.longitude
+        });
+
+        if (kilometers <= +req.query.around)
+          nearby.push(establishment);
       });
 
-      if (kilometers <= +request.query.around)
-        nearby.push(establishment);
+      res.send(nearby);
     });
-
-    response.send(nearby);
   });
 });
